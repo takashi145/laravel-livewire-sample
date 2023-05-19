2a09e72be9431dba96154c5662b19820624e8f15
takashi145 <t.shuhei0203@gmail.com>
2023-05-20 06:49:15 +0900

    Fix x-cloak functionality and modify flash-message component

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,4 +1,4 @@
-@props(['duration' => 5000])
+@props(['duration' => 3000])
 
 @php
     $type = session('flashMessage.type');
@@ -10,11 +10,10 @@
 @endphp
 
 @if (session()->has('flashMessage.message'))
-<div x-data="{show: true}"
-    x-init="setTimeout(() => {
-        show = false;
-    }, {{ $duration }})"
+<div x-data="{show: false}"
+    x-init="setTimeout(() => { show = true; }, 100); setTimeout(() => { show = false; }, {{ $duration }})"
     x-show="show"
+    x-on:click="show = false"
     x-transition:enter="transition ease-out duration-300 transform"
     x-transition:enter-start="translate-x-full"
     x-transition:enter-end="translate-x-0"
@@ -22,7 +21,7 @@
     x-transition:leave-start="translate-x-0"
     x-transition:leave-end="translate-x-full"
     x-cloak
-    class="fixed z-50 top-6 right-3 max-w-full text-white px-4 py-2 rounded shadow-md {{ $classes[$type] }}">
+    class="fixed z-50 top-6 right-3 max-w-full text-white px-4 py-2 rounded shadow-md hover:cursor-pointer {{ $classes[$type] }}">
     <div>{{ session('flashMessage.message') }}</div>
 </div>
 @endif
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,7 +9,7 @@
                 </div>
             </div>
 
-            <div x-data="{dropdown: false, open_menu: false}" class="max-w-7xl flex items-center order-2 relative">
+            <div x-data="{dropdown: false, open_menu: false}" x-cloak class="max-w-7xl flex items-center order-2 relative">
                 <button type="button" x-on:click="dropdown = !dropdown" class="flex text-gray-700 dark:text-gray-300 hover:underline mr-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
